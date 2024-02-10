1ab050a2d47259b95f7c9b67c7190174b2dd682e
Joo98e <jtbeok@gmail.com>
2024-02-10 10:52:59 +0900

    feature: board controller, service

---
+++ src/main/kotlin/com/joo98e/practicejoo98eserverkt/domain/board/controller/BoardQueryController.kt
@@ -0,0 +1,6 @@
+package com.joo98e.practicejoo98eserverkt.domain.board.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class BoardQueryController()
---
+++ src/main/kotlin/com/joo98e/practicejoo98eserverkt/domain/board/service/BoardQueryService.kt
@@ -0,0 +1,7 @@
+package com.joo98e.practicejoo98eserverkt.domain.board.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class BoardQueryService {
+}

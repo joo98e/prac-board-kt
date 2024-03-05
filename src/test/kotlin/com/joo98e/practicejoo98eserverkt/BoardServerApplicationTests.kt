9b0b8fec5d84cce145de3aef55ed2ad5e5de50e6
Joo98e <jtbeok@gmail.com>
2024-03-05 22:11:29 +0900

    feature : kotest, runner (ide plugin setup 필요)

--- src/test/kotlin/com/joo98e/practicejoo98eserverkt/BoardServerApplicationTests.kt
+++ src/test/kotlin/com/joo98e/practicejoo98eserverkt/BoardServerApplicationTests.kt
@@ -1,13 +1,16 @@
 package com.joo98e.practicejoo98eserverkt
 
-import org.junit.jupiter.api.Test
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class BoardServerApplicationTests {
-
-    @Test
-    fun contextLoads() {
+class BoardServerApplicationTests : BehaviorSpec({
+    Given("a test") {
+        When("running a test") {
+            Then("it should pass") {
+                true shouldBe true
+            }
+        }
     }
-
-}
+})

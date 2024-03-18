e7679b1fad14b1a7cc77dcbfda3bcb4fedb8599b
Joo98e <jtbeok@gmail.com>
2024-03-18 22:40:47 +0900

    feature : board command controllerˆ Update

--- src/main/kotlin/com/joo98e/practicejoo98eserverkt/domain/board/controller/BoardCommandController.kt
+++ src/main/kotlin/com/joo98e/practicejoo98eserverkt/domain/board/controller/BoardCommandController.kt
@@ -1,15 +1,22 @@
 package com.joo98e.practicejoo98eserverkt.domain.board.controller
 
 import com.joo98e.practicejoo98eserverkt.domain.board.dto.BoardCreateCommand
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import com.joo98e.practicejoo98eserverkt.domain.board.dto.BoardUpdateCommand
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class BoardCommandController {
     @PostMapping("/boards")
     fun createBoard(
         @RequestBody command: BoardCreateCommand,
+    ): Long {
+        return 1L
+    }
+
+    @PutMapping("/boards/{id}")
+    fun updateBoard(
+        @PathVariable id: Long,
+        @RequestBody command: BoardUpdateCommand,
     ): Long {
         return 1L
     }
